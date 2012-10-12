e6ffe15bc6889a258d7ffa307805a902612d961f
Simon Hengel <sol@typeful.net>
2012-10-12 13:15:53 +0200

    Add driver

---
+++ driver/Main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import           System.IO
+import           System.Exit
+import           System.Environment
+
+import           Text.Markdown.Unlit (unlit)
+
+main :: IO ()
+main = getArgs >>= \args -> case args of
+  -- GHC calls unlit like so:
+  --
+  -- > unlit -h label Foo.lhs /tmp/somefile
+  --
+  -- The label is meant to be used in line pragmas, like so:
+  --
+  -- #line 1 "label"
+  --
+  ["-h", _, infile, outfile] ->
+    fmap unlit (readFile infile) >>= writeFile outfile
+  _ -> do
+    name <- getProgName
+    hPutStrLn stderr ("usage: " ++ name ++ " -h label infile outfile")
+    exitFailure
